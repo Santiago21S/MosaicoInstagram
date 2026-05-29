ca49488881a58f1c0bdad7f65b71cd7787ccf943
Santiago Sanabria <santiago2004_s_b@outlook.com>
2026-05-28 18:20:16 -0600

    .env.example

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # MariaDB usa el backend de mysql
         'NAME': os.environ.get('DB_NAME', 'mosaico_db'),
         'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
+        'PASSWORD': os.environ.get('DB_PASSWORD', '12345'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
